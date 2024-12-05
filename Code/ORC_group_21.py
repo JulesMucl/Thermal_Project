a7e117819f2d32d92efef735ec7dabb512664c65
JulesMucl <jules.moreau@student.uclouvain.be>
2024-12-05 16:24:12 +0100

    Beaucouuuuup de correction

--- Code/ORC_group_21.py
+++ Code/ORC_group_21.py
@@ -22,36 +22,62 @@ class ORC(object):
     def __init__(self,inputs,parameters,display):
 
         self.display             = display
-        self.T_max               = parameters['T_max']
-        self.p_3                 = parameters['p_3']
-        self.r_pump_1            = parameters['r_pump_1'] # Pour moi on peut pas les imposer !!!!
-        self.r_pump_2            = parameters['r_pump_2'] # Pour moi on peut pas les imposer !!!! Car redondant avec d'aures infos -> on va être bloqué ! 
-        self.fluid               = parameters['fluid']
-        self.k_cc_ex1            = parameters['k_cc_ex1']
-        self.k_cc_ex2            = parameters['k_cc_ex2']
-        self.T_cold_fluid_in     = parameters['T_cold_fluid_in']
-        self.T_cold_fluid_out    = parameters['T_cold_fluid_out']
-        self.cold_fluid          = parameters['cold_fluid']
+
+        # HOT Fluid
         self.hot_fluid           = parameters['hot_fluid']
+        self.T_8                 = parameters['T_8']
+        self.T_7                 = parameters['T_7']
+        self.T_9                 = parameters['T_9'] 
+        self.T_pinch_ex_I        = parameters['T_pinch_ex_I']
+        self.T_pinch_ex_II       = parameters['T_pinch_ex_II']   
+        self.p_HF                 = parameters['p_HF']
         self.dot_m_ex            = parameters['dot_m_ex']   # mass flow du fluid chaud  10 kg/s
-        self.T_hot_fluid_in_II   = parameters['T_hot_fluid_in_II'] 
-        self.p_hot_fluid         = parameters['p_hot_fluid']
-        self.eta_pump_1          = parameters['eta_pump_1']
-        self.eta_pump_2          = parameters['eta_pump_2']
-        self.T_cd_subcool        = parameters['T_cd_subcool']
-        self.T_surchauffe        = parameters['T_surchauffe']
-        self.eta_is_T            = parameters['eta_is_T']
-        self.m_tot               = parameters['m_tot']
+
+        # Cold Fluid
+        self.cold_fluid          = parameters['cold_fluid']
         self.m_dot_CF            = parameters['m_dot_CF']
         self.T_10                = parameters['T_10']
         self.T_11                = parameters['T_11']
         self.p_CF                = parameters['p_CF']
+        self.T_pinch_cd         = parameters['T_pinch_cd']
+        
+        # Etat 3
+        self.T_max               = parameters['T_max']
 
-        
-        
+        # General 
+        self.fluid               = parameters['fluid']
+        self.k_cc_ex1            = parameters['k_cc_ex1']
+        self.k_cc_ex2            = parameters['k_cc_ex2']
+
+        # Pump
+        self.eta_pump_1          = parameters['eta_pump_1']
+        self.eta_pump_2          = parameters['eta_pump_2']
+        self.T_cd_subcool        = parameters['T_cd_subcool']
+
+        # Turbine
+        self.eta_is_T            = parameters['eta_is_T']
+
+        # ETAT REF
+        self.p_ref, self.T_ref = parameters['p_ref'],   parameters['T_ref']
+        self.h_ref = PropsSI('H','P',self.p_ref,'T',self.T_ref,self.fluid)
+        self.s_ref = PropsSI('S','P',self.p_ref,'T',self.T_ref,self.fluid)
+
+        # Contraintes
+        self.x5_lim    = 0.88
+        self.x6        = 0
+        self.p_7       = self.p_HF
+
+        # Impose
+
+        self.T_surchauffe = parameters['T_surchauffe']
+        self.m_tot = parameters['m_tot']
+
+        # Guess
+        self.p_1_guess = parameters['p_1_guess']
+        self.p_2_guess = parameters['p_2_guess']
+        self.p_5_guess = parameters['p_5_guess']
+
        
-
-        self.T_pinch_ex = 1
 
 
         # ETAT REF
@@ -64,18 +90,9 @@ class ORC(object):
         # Contraintes
         self.x5_lim = 0.88
         self.x6 = 0
-        self.T_7 = self.T_hot_fluid_in_II
-        self.p_7 = self.p_hot_fluid
 
        
-    def T_pinch_cd(self,T5, T_cold_in, T_cold_out):
-        """
-        Retourne la température T_pinch du condensateur
-        Ce fait via des itérations
-
-        A faire 
-        """
-        return 1
+  
 
     def exergie(self,h_i,s_i):
 
@@ -126,10 +143,36 @@ class ORC(object):
 
 
             #region ETAT 8
+        self.h_8 = PropsSI("H","T",self.T_8,"P",self.p_7,self.hot_fluid)
+        self.s_8 = PropsSI("S","T",self.T_8,"P",self.p_7,self.hot_fluid)
+        self.e_8 = self.exergie(self.h_8,self.s_8)
+        self.x_8 = PropsSI("Q","T",self.T_8,"P",self.p_7,self.hot_fluid)
 
             #endregion etat 8
 
-        
+            #region ETAT 9
+
+        self.h_9 = PropsSI("H","T",self.T_9,"P",self.p_7,self.hot_fluid)
+        self.s_9 = PropsSI("S","T",self.T_9,"P",self.p_7,self.hot_fluid)
+        self.e_9 = self.exergie(self.h_9,self.s_9)
+        self.x_9 = PropsSI("Q","T",self.T_9,"P",self.p_7,self.hot_fluid)
+            #endregion etat 9
+
+            #region ETAT 10
+        self.h_10 = PropsSI("H","T",self.T_10,"P",self.p_CF,self.cold_fluid)
+        self.s_10 = PropsSI("S","T",self.T_10,"P",self.p_CF,self.cold_fluid)
+        self.e_10 = self.exergie(self.h_10,self.s_10)
+        self.x_10 = PropsSI("Q","T",self.T_10,"P",self.p_CF,self.cold_fluid)
+            #endregion etat 10
+
+            #region ETAT 11
+        self.h_11 = PropsSI("H","T",self.T_11,"P",self.p_CF,self.cold_fluid)
+        self.s_11 = PropsSI("S","T",self.T_11,"P",self.p_CF,self.cold_fluid)
+        self.e_11 = self.exergie(self.h_11,self.s_11)
+        self.x_11 = PropsSI("Q","T",self.T_11,"P",self.p_CF,self.cold_fluid)
+            #endregion etat 11
+
+
         
 
         # On pose les pressions en premier guess
@@ -174,7 +217,7 @@ class ORC(object):
             # Via turbine : 
             #region ETAT 3_PRIME
             self.h_3_prime_s = PropsSI("H","P",self.p_5_guess,"S",self.s_3,self.fluid)
-            self.h_3_prime = self.h_3 - self.eta_is_T * (self.h_3 - self.h_3_prim_s)
+            self.h_3_prime = self.h_3 - self.eta_is_T * (self.h_3 - self.h_3_prime_s)
             self.T_3_prime = PropsSI("T","P",self.p_5_guess,"H",self.h_3_prime,self.fluid)
             self.s_3_prime = PropsSI("S","P",self.p_5_guess,"H",self.h_3_prime,self.fluid)
             self.e_3_prime = self.exergie(self.h_3_prime,self.s_3_prime)
@@ -183,7 +226,7 @@ class ORC(object):
 
             #region ETAT 4_PRIME
             self.h_4_prime_s = PropsSI("H","P",self.p_5_guess,"S",self.s_4,self.fluid)
-            self.h_4_prime = self.h_4 - self.eta_is_T * (self.h_4 - self.h_4_prim_s)
+            self.h_4_prime = self.h_4 - self.eta_is_T * (self.h_4 - self.h_4_prime_s)
             self.T_4_prime = PropsSI("T","P",self.p_5_guess,"H",self.h_4_prime,self.fluid)
             self.s_4_prime = PropsSI("S","P",self.p_5_guess,"H",self.h_4_prime,self.fluid)
             self.e_4_prime = self.exergie(self.h_4_prime,self.s_4_prime)
@@ -198,11 +241,12 @@ class ORC(object):
 
             #region ETAT 1
             def T_out_pumpI(T1_guess) :
-                cp_average = self.CP_av(T1_guess,self.T_2,self.p_5_guess,self.p_1_guess,self.fluid)
+                cp_average = self.CP_av(self.T_6 , T1_guess , self.p_5_guess , self.p_1_guess , self.fluid)
                 
                 return T1_guess - self.T_6 - (self.p_1_guess - self.p_5_guess) / (self.eta_pump_1 * cp_average)
 
             self.T_1 = fsolve(T_out_pumpI, self.T_6*1.02)[0]
+            print("T1 === ",self.T_1-273.15,"°C")
             self.h_1 = PropsSI("H","P",self.p_1_guess,"T",self.T_1,self.fluid)
             self.s_1 = PropsSI("S","P",self.p_1_guess,"T",self.T_1,self.fluid)
             self.e_1 = self.exergie(self.h_1,self.s_1)
@@ -211,11 +255,11 @@ class ORC(object):
 
             #region ETAT 2
             def T_out_pumpII(T2_guess) :
-                cp_average2 = self.CP_av(T2_guess,self.T_3,self.p_1_guess,self.p_2_guess,self.fluid)
+                cp_average2 = self.CP_av(self.T_1 , T2_guess , self.p_1_guess , self.p_2_guess , self.fluid)
                 
                 return T2_guess - self.T_1 - (self.p_2_guess - self.p_1_guess) / (self.eta_pump_2 * cp_average2)
             
-            self.T_2 = fsolve(T_out_pumpII, self.T_1*1.02)[0]
+            self.T_2 = fsolve(T_out_pumpII, self.T_1*1.05)[0]
             self.h_2 = PropsSI("H","P",self.p_2_guess,"T",self.T_2,self.fluid)
             self.s_2 = PropsSI("S","P",self.p_2_guess,"T",self.T_2,self.fluid)
             self.e_2 = self.exergie(self.h_2,self.s_2)
@@ -242,28 +286,67 @@ class ORC(object):
 
                 return T6i - Tcsi - self.T_pinch_cd
 
-            # A CORRIGER !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            # def Etat_i_evaporator_I(self,p_3_guess):
 
-            #     h_hs_ex = self.h_8
-            #     h_cs_su = self.h_2
+            def Etat_i_evaporator_I(self,p_3_guess):
 
-            #     T_hs_i = PropsSI('T', 'P', p_3_guess , 'Q', 0, self.fluid) # T_hs_sat_liq
-            #     h_hs_i = PropsSI('H', 'P', p_3_guess , 'Q', 0, self.fluid) # h_hs_sat_liq
 
-            #     h_cs_i = (1/self.ratio_I) * (h_hs_i- h_hs_ex) + h_cs_su
-            #     T_cs_i = PropsSI('T', 'H', h_cs_i, 'P', self.p_7, self.hot_fluid)
+                h_hs_ex = self.h_8
+                h_hs_su = self.h_7
+                h_cs_su = self.h_2
+                h_cs_ex = self.h_3
 
-            #     return h_hs_i, h_cs_i, T_hs_i, T_cs_i
+
+                T_cs_i = PropsSI('T', 'P', p_3_guess , 'Q', 0, self.fluid) 
+                h_cs_i = PropsSI('H', 'P', p_3_guess , 'Q', 0, self.fluid) 
+
+                h_hs_i = h_hs_su - (self.dot_m_cf/self.m_1) * (h_cs_ex - h_cs_su) 
+                T_hs_i = PropsSI('T', 'H', h_hs_i, 'P', self.p_7, self.hot_fluid)
+
+
+                return h_hs_i, h_cs_i, T_hs_i, T_cs_i
             
-            # def Iter_function_evaportor_I(self,p_3_guess):
+            def Iter_function_evaportor_I(self,p_3_guess):
                     
-            #     h_hs_i, h_cs_i, T_hs_i, T_cs_i = self.Etat_i(p_3_guess)
+                h_hs_i, h_cs_i, T_hs_i, T_cs_i = self.Etat_i(p_3_guess)
             
-            #     return T_cs_i - T_hs_i - self.T_pinch_ex_I
+                return T_hs_i - T_cs_i - self.T_pinch_ex_I
             
+            def Etat_i_evaporator_II(self,p_1_guess):
+
+                h_hs_ex = self.h_9
+                h_hs_su = self.h_8
+                h_cs_su = self.h_1
+                h_cs_ex = self.h_4
+
+
+                T_cs_i = PropsSI('T', 'P', p_1_guess , 'Q', 0, self.fluid) 
+                h_cs_i = PropsSI('H', 'P', p_1_guess , 'Q', 0, self.fluid) 
+
+                h_hs_i = h_hs_su - (self.dot_m_cf/self.m_1) * (h_cs_ex - h_cs_su) 
+                T_hs_i = PropsSI('T', 'H', h_hs_i, 'P', self.p_7, self.hot_fluid)
+
+
+                return h_hs_i, h_cs_i, T_hs_i, T_cs_i
+            
+            def Iter_function_evaportor_II(self,p_1_guess):
+                    
+                h_hs_i, h_cs_i, T_hs_i, T_cs_i = self.Etat_i_evaporator_II(p_1_guess)
+            
+                return T_hs_i - T_cs_i - self.T_pinch_ex_II
             
             #endregion checks pinch
+
+            initial_guess = [self.p_1_guess, self.p_2_guess, self.p_5_guess]
+
+            soluce = fsolve(cycle, initial_guess)
+            p_1, p_2, p_5 = soluce[0], soluce[1], soluce[2]
+
+            return p_1, p_2, p_5
+
+        self.p_1, self.p_2, self.p_5 = cycle(self.p_1_guess, self.p_2_guess, self.p_5_guess)
+
+
+            
 
 
         # Faire une matrice comme au HMW 3 : plus petite 
@@ -301,6 +384,10 @@ class ORC(object):
             print("P3 === ",self.p_3/1000,"kPa")
             print("T3 === ",self.T_3)
             print("e3 === ",self.e_3/1000,"kJ/kg")
+
+            print("P1 === ",self.p_1/1000,"kPa")
+            print("P2 === ",self.p_2/1000,"kPa")
+            print("P5 === ",self.p_5/1000,"kPa")
    
 
 
